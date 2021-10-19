bd9d8a15a04cbf1f32255eaeb5cd101f7cf1ef93
berry-peachy <karla.rodriguez@revature.net>
2021-10-19 17:51:21 -0400

    Tested element creation

--- DL/DBRepo.cs
+++ DL/DBRepo.cs
@@ -12,8 +12,6 @@ namespace DL
     {
         private PokeMatchDb _context;
 
-    
-
         //Element
         public async Task<Element> AddElementAsync (Element ele)
         {
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -32,6 +32,8 @@ namespace WebAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
             });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
