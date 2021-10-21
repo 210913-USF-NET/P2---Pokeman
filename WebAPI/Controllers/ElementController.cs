4fbf04b4baeaac8af12cdb069bb05ebdc91506df
Jean Duong <bduong@mymail.mines.edu>
2021-10-21 15:15:09 -0600

    Larry's fault

--- WebAPI/Controllers/ElementController.cs
+++ WebAPI/Controllers/ElementController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace WebAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ElementController : Controller
     {
         private IBL _bl;
