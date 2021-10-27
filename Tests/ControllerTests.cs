7a3628505b6b71faae2e4aee1538fc4d4619428f
berry-peachy <karla.rodriguez@revature.net>
2021-10-27 18:55:50 -0400

    bao update

---
+++ Tests/ControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using DL;
+using WebAPI.Controllers;
+using Models;
+using Microsoft.EntityFrameworkCore;
+using BL;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Configuration;
+
+//namespace Tests
+//{
+//    public class Tests
+//    {
+//        //private readonly DbContextOptions<PokeMatchDb> options;
+//        private readonly IBL configuration;
+
+//        [Fact]
+        
+//        public async void Get_GetListOfElements()
+//        {
+//            var configure = GetConfig();
+
+//            var sut = new ElementController(configuration);
+
+//            var result = await sut.Get();
+
+//            Assert.Equal(2, result.Count());
+
+//        }
+
+//        private IConfiguration GetConfig()
+//        {
+//            var builder = new ConfigurationBuilder()
+//              .SetBasePath(Directory.GetCurrentDirectory())
+//              .AddJsonFile("appsettings.json", true, true)
+//              .AddEnvironmentVariables();
+
+//            return builder.Build();
+//        }
+//    }
+//}
